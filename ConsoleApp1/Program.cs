ae191c79c515e72e9b24784aa208bb9bcd3b588a
СГК ГАПОУ <sgk.gapou@outlook.com>
2022-09-13 08:56:24 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Добро пожаловать в кукулятор v1");
+            Console.WriteLine("Введите первое число");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите второе число");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Какое действие вы хотите выполнить?");
+            var enter = Console.ReadKey();
+            char enter_key = enter.KeyChar;
+
+            if(enter_key == '+')
+            {
+                Console.WriteLine(a + b);
+            }
+            else if(enter_key == '-')
+            {
+                Console.WriteLine(a - b);
+            }
+            else if(enter_key == '*')
+            {
+                Console.WriteLine(a * b);
+            }
+            else if(enter_key == '/')
+            {
+                Console.WriteLine(a / b);
+            }
+            else
+            {
+                Console.WriteLine("Неверно выбрано действие");
+            }
+
+
+        }
+    }
+}
